c7e653b05f13dab58a2898b82cc5d4b29c354706
kevintran4u <kevintran4u@gmail.com>
2021-08-19 07:40:19 -0700

    removed GTM- prepend for tag manager code

--- app/themes/sage/resources/views/partials/head.blade.php
+++ app/themes/sage/resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-{!! $tag_manager !!}');</script>
+})(window,document,'script','dataLayer','{!! $tag_manager !!}');</script>
 <!-- End Google Tag Manager -->
 @endif
 <script src="/app/themes/sage/resources/assets/scripts/vanilla-tilt.min.js."></script>
